95e25ed00c2f9efea549130c9f7c81d5b79afc35
YuhBish <70145942+YuhBish@users.noreply.github.com>
2020-11-02 13:20:39 +0100

    Create attupprepa.py

---
+++ attupprepa.py
@@ -0,0 +1,14 @@
+import random
+
+gissning = float (input("Nummer, NU "))
+
+while gissning !=8:
+    print ("Ha!, du suger")
+    if gissa < 8:
+        print ("nu blev de lite högt här")
+        break
+    elif gissning = 8:
+        print ("för lågt....")
+        break
+else:
+    print ("fan va tur du skulle ha då")
