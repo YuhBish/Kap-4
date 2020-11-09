d5b64f56f68273ea802cab7b2c49bf7a846c7385
YuhBish <70145942+YuhBish@users.noreply.github.com>
2020-11-09 14:52:18 +0100

    asd

---
+++ Uppgift 4.1.py
@@ -0,0 +1,6 @@
+print ('[', end='')
+k = 1 
+while k < 6:
+    print(f'{k:2}', end='')
+    k = k + 2 
+print(']')
--- copypaste.py
+++ copypaste.py
@@ -2,11 +2,22 @@ import random
 
 guess = 0
 guesses = 0
-number = random.randint (0, 100)
+number = random.randint (0, 1000000)
 
 while guess != number:
     guesses += 1
-    guess = random.randint (0, 100)
+    guess = random.randint (0, 1000000)
 print (f"Amount of tries: {guesses} ")
 print (f"The number was: {number}")
 
+if guesses > 3000000:
+    print ("wow many gues")
+
+if guesses < 10000:
+    print ("wow few gues")
+
+if number > 999000:
+    print ("wow big nomber")
+
+if number < 10000:
+    print ("wow smol nomber")
